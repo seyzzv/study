cc2628aac33fc99180ff3aba15df2d1641f71882
seyzzv <seyzzv@gmail.com>
2025-10-23 20:09:13 +0900

    언어별 폴더 이동

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++
@@ -1,5 +0,0 @@
-def solution(seoul):
-    n = seoul.index('Kim')
-    answer = f'김서방은 {n}에 있다'
-    return answer
-
--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    answer = 0
-    n = str(n)
-    for i in range(len(n)):
-        answer += int(n[i])
-    return answer
