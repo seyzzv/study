b56d6a2621380c0224fa952ad3ee2d6a9ff5f2c4
seyzzv <seyzzv@gmail.com>
2025-11-02 11:20:32 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 9.22 MB -BaekjoonHub

--- Python3/프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ Python3/프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,8 +1,2 @@
 def solution(n_str):
-    answer = '' 
-    isTF = False
-    for i in n_str:
-        if i != '0': isTF = True
-        if isTF: answer += i
-    if answer == '': answer = '0'
-    return answer
+    return n_str.lstrip('0')
