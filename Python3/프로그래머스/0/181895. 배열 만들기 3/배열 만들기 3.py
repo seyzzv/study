49e94e3d42640630b5213b9b2eff1ddadd66a31f
seyzzv <seyzzv@gmail.com>
2025-11-15 21:00:34 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.78 ms, Memory: 14.6 MB -BaekjoonHub

--- Python3/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ Python3/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,6 +1,4 @@
 def solution(arr, intervals):
     a1, b1 = intervals[0]
     a2, b2 = intervals[1]
-    first_part = arr[a1:b1+1]
-    second_part = arr[a2:b2+1]
-    return first_part + second_part
+    return arr[a1:b1+1] + arr[a2:b2+1]
