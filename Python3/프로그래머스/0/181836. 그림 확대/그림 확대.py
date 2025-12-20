e6f1cf163b62670550ad889c5177ea302a21c9ba
seyzzv <seyzzv@gmail.com>
2025-12-20 15:16:59 +0900

    [level 0] Title: 그림 확대, Time: 0.14 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -0,0 +1,6 @@
+def solution(picture, k):
+    answer = []
+    for i in range(len(picture)):
+        for _ in range(k):
+            answer.append(picture[i].replace('.', '.' * k).replace('x', 'x' * k))
+    return answer
