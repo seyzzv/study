e5a0ec00f7779fd32a6f684018cef0743f8f58fd
seyzzv <seyzzv@gmail.com>
2026-01-03 17:00:39 +0900

    [level 2] Title: 올바른 괄호, Time: 5.57 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    count = 0
+    for ch in s:
+        if ch == '(':
+            count += 1
+        else:
+            count -= 1
+            if count < 0:
+                return False
+    if count != 0:
+        return False
+    return True
