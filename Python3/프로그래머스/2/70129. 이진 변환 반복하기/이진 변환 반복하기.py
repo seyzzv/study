1404dc67c0c8bd56703f6cf16f563c9b81e953bb
seyzzv <seyzzv@gmail.com>
2026-01-10 09:13:36 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.48 ms, Memory: 9.27 MB -BaekjoonHub

--- Python3/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ Python3/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,13 +1,8 @@
 def solution(s):
-    trans_cnt = 0
-    del_cnt = 0
+    a, b = 0, 0
     while s != '1':
-        zero_cnt = 0
-        for ch in s:
-            if ch == '0':
-                zero_cnt += 1
-        del_cnt += zero_cnt
-        s = s.replace('0', '')
-        s = bin(len(s))[2:]
-        trans_cnt += 1
-    return [trans_cnt, del_cnt]
+        a += 1
+        num = s.count('1')
+        b += len(s) - num
+        s = bin(num)[2:]
+    return [a, b]
