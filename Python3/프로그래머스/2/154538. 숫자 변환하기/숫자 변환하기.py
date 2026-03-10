15cee12112ef54f6234665937e87bab58f14c75a
seyzzv <seyzzv@gmail.com>
2026-03-10 12:32:41 +0900

    [level 2] Title: 숫자 변환하기, Time: 312.24 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(x, y, n):
+    visited = [False] * (y + 1)
+    q = deque([(x, 0)])
+    visited[x] = True
+    
+    while q:
+        cur, cnt = q.popleft()
+        
+        if cur == y:
+            return cnt
+        
+        for nxt in (cur + n, cur * 2, cur * 3):
+            if nxt <= y and not visited[nxt]:
+                visited[nxt] = True
+                q.append((nxt, cnt + 1))
+    
+    return -1
