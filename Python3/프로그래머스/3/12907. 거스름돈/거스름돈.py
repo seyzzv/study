14cf7178c15cbcf281c06dffd56d7c8443cfaf50
seyzzv <seyzzv@gmail.com>
2026-05-20 11:37:08 +0900

    [level 3] Title: 거스름돈, Time: 282.73 ms, Memory: 12.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,9 @@
+def solution(n, money):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    
+    for coin in money:
+        for i in range(coin, n + 1):
+            dp[i] = (dp[i] + dp[i - coin]) % 1000000007
+            
+    return dp[n]
