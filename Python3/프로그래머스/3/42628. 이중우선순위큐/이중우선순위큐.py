5a0b676e30275edcc986ad954d0f30dde8c0c216
seyzzv <seyzzv@gmail.com>
2026-05-25 10:25:14 +0900

    [level 3] Title: 이중우선순위큐, Time: 264.79 ms, Memory: 15.5 MB -BaekjoonHub

--- Python3/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ Python3/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,31 +1,19 @@
 import heapq
 
 def solution(operations):
-    min_h, max_h = [], []
-    valid = {}
-
-    for i, op in enumerate(operations):
+    h = []
+    
+    for op in operations:
         cmd, num = op.split()
         num = int(num)
-
+        
         if cmd == 'I':
-            heapq.heappush(min_h, (num, i))
-            heapq.heappush(max_h, (-num, i))
-            valid[i] = True
-        elif num == 1:
-            while max_h:
-                _, idx = heapq.heappop(max_h)
-                if idx in valid:
-                    del valid[idx]
-                    break
-        else:
-            while min_h:
-                _, idx = heapq.heappop(min_h)
-                if idx in valid:
-                    del valid[idx]
-                    break
-
-    while min_h and min_h[0][1] not in valid: heapq.heappop(min_h)
-    while max_h and max_h[0][1] not in valid: heapq.heappop(max_h)
-
-    return [-max_h[0][0], min_h[0][0]] if valid else [0, 0]
+            heapq.heappush(h, num)
+        elif h:
+            if num == 1:
+                h.remove(max(h))
+                heapq.heapify(h)
+            else:
+                heapq.heappop(h)
+                
+    return [max(h), h[0]] if h else [0, 0]
