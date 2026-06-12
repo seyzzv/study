da5d457a89d2b83aa392e367c4e592cf5307f511
seyzzv <seyzzv@gmail.com>
2026-06-12 12:14:29 +0900

    [level 3] Title: 주사위 고르기, Time: 84.47 ms, Memory: 11.6 MB -BaekjoonHub

--- Python3/프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ Python3/프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,5 +1,4 @@
 from itertools import combinations
-from collections import Counter
 from bisect import bisect_left
 
 def solution(dice):
@@ -12,23 +11,32 @@ def solution(dice):
     for a_idx in combinations(range(n), half):
         b_idx = tuple(idx_set - set(a_idx))
         
-        a_dp = Counter([0])
+        a_dp = [0] * 501
+        a_dp[0] = 1
         for i in a_idx:
-            nxt = Counter()
-            for s, c in a_dp.items():
-                for v in dice[i]:
-                    nxt[s + v] += c
+            nxt = [0] * 501
+            for s in range(501):
+                if a_dp[s]:
+                    for v in dice[i]:
+                        nxt[s + v] += a_dp[s]
             a_dp = nxt
             
-        b_dp = Counter([0])
+        b_dp = [0] * 501
+        b_dp[0] = 1
         for i in b_idx:
-            nxt = Counter()
-            for s, c in b_dp.items():
-                for v in dice[i]:
-                    nxt[s + v] += c
+            nxt = [0] * 501
+            for s in range(501):
+                if b_dp[s]:
+                    for v in dice[i]:
+                        nxt[s + v] += b_dp[s]
             b_dp = nxt
 
-        b_sc, b_cnt = zip(*sorted(b_dp.items()))
+        b_sc, b_cnt = [], []
+        for s in range(501):
+            if b_dp[s]:
+                b_sc.append(s)
+                b_cnt.append(b_dp[s])
+
         b_cum = []
         curr = 0
         for c in b_cnt:
@@ -36,10 +44,11 @@ def solution(dice):
             b_cum.append(curr)
 
         w_cnt = 0
-        for a_sc, a_c in a_dp.items():
-            pos = bisect_left(b_sc, a_sc)
-            if pos > 0:
-                w_cnt += b_cum[pos - 1] * a_c
+        for s in range(501):
+            if a_dp[s]:
+                pos = bisect_left(b_sc, s)
+                if pos > 0:
+                    w_cnt += b_cum[pos - 1] * a_dp[s]
 
         if w_cnt > max_w:
             max_w = w_cnt
