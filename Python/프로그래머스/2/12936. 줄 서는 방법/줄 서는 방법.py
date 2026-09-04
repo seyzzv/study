b70e91dd85db1e0da044eb799a4896edd17e33bb
seyzzv <seyzzv@gmail.com>
2026-09-05 05:58:40 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.01 ms, Memory: 11 MB -BaekjoonHub

---
+++ Python/프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,17 @@
+import math
+
+def solution(n, k):
+    answer = []
+    numbers = list(range(1, n + 1))
+    
+    k -= 1
+    
+    for i in range(n, 0, -1):
+        fact = math.factorial(i - 1)
+        
+        idx = k // fact
+        answer.append(numbers.pop(idx))
+        
+        k %= fact
+        
+    return answer
